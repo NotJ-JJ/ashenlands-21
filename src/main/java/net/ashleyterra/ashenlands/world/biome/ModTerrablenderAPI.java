e16040a0d97af32da7f6fbd2836526436a640ae8
NotJJ <z.notjj@gmail.com>
2026-03-12 00:09:22 +1100

    finalised datagen

--- src/main/java/net/ashleyterra/ashenlands/world/biome/ModTerrablenderAPI.java
+++ src/main/java/net/ashleyterra/ashenlands/world/biome/ModTerrablenderAPI.java
@@ -1,10 +1,17 @@
 package net.ashleyterra.ashenlands.world.biome;
 
+import net.ashleyterra.ashenlands.AshenLands;
+import net.ashleyterra.ashenlands.world.biome.surface.ModMaterialRules;
+import net.minecraft.util.Identifier;
+import terrablender.api.Regions;
+import terrablender.api.SurfaceRuleManager;
 import terrablender.api.TerraBlenderApi;
 
 public class ModTerrablenderAPI implements TerraBlenderApi {
     @Override
     public void onTerraBlenderInitialized() {
+        Regions.register(new ModOverworldRegion(Identifier.of(AshenLands.MOD_ID,"overworld"),5));
 
+        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD,AshenLands.MOD_ID, ModMaterialRules.makeRules());
     }
 }
--- src/main/java/net/ashleyterra/ashenlands/world/biome/surface/ModMaterialRules.java
+++ src/main/java/net/ashleyterra/ashenlands/world/biome/surface/ModMaterialRules.java
@@ -1,4 +1,32 @@
 package net.ashleyterra.ashenlands.world.biome.surface;
 
+import net.ashleyterra.ashenlands.world.biome.ModBiomes;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.gen.surfacebuilder.MaterialRules;
+
 public class ModMaterialRules {
+    private static final MaterialRules.MaterialRule DIRT = makeStateRule(Blocks.DIRT);
+    private static final MaterialRules.MaterialRule GRASS_BLOCK = makeStateRule(Blocks.GRASS_BLOCK);
+    private static final MaterialRules.MaterialRule IRON_BLOCK = makeStateRule(Blocks.IRON_BLOCK);
+    private static final MaterialRules.MaterialRule EMERALD_BLOCK = makeStateRule(Blocks.EMERALD_BLOCK);
+
+    public static MaterialRules.MaterialRule makeRules() {
+        MaterialRules.MaterialCondition isAtOrAboveWaterLevel = MaterialRules.water(-1, 0);
+
+        MaterialRules.MaterialRule grassSurface = MaterialRules.sequence(MaterialRules.condition(isAtOrAboveWaterLevel, GRASS_BLOCK), DIRT);
+
+        return MaterialRules.sequence(
+                MaterialRules.sequence(MaterialRules.condition(MaterialRules.biome(ModBiomes.ASHEN_LAND),
+                                MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, IRON_BLOCK)),
+                        MaterialRules.condition(MaterialRules.STONE_DEPTH_CEILING, EMERALD_BLOCK)),
+
+                // Default to a grass and dirt surface
+                MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, grassSurface)
+        );
+    }
+
+    private static MaterialRules.MaterialRule makeStateRule(Block block) {
+        return MaterialRules.block(block.getDefaultState());
+    }
 }
